58ed8e12e511232b7e297c6383044612695e9653
Jeff Chen <jeff.chen@circleci.com>
2022-04-19 15:33:05 -0700

    Publish v1.18.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/go:1.17.9
-docker push cimg/go:1.17
-docker push cimg/go:1.17.9-node
-docker push cimg/go:1.17-node
-docker push cimg/go:1.17.9-browsers
-docker push cimg/go:1.17-browsers
-
-docker push cimg/go:1.18.1
 docker push cimg/go:1.18
-docker push cimg/go:1.18.1-node
+docker push cimg/go:1.18.1
 docker push cimg/go:1.18-node
-docker push cimg/go:1.18.1-browsers
+docker push cimg/go:1.18.1-node
 docker push cimg/go:1.18-browsers
+docker push cimg/go:1.18.1-browsers
