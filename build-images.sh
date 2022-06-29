268c97429e8838a5c69242584c730f8909c110df
Jeff Chen <jeff.chen@circleci.com>
2022-06-29 13:03:55 -0700

    updated tests and trying to push to ccitest

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.17/Dockerfile -t cimg/go:1.17.11 -t cimg/go:1.17 .
-docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.11-node -t cimg/go:1.17-node .
-docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.11-browsers -t cimg/go:1.17-browsers .
+docker build --file 1.17/Dockerfile -t ccitest/testing:1.17 .
+# docker build --file 1.17/node/Dockerfile -t cimg/go:1.17.11-node -t cimg/go:1.17-node .
+# docker build --file 1.17/browsers/Dockerfile -t cimg/go:1.17.11-browsers -t cimg/go:1.17-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.17
-docker push cimg/go:1.17.11
-docker push cimg/go:1.17-node
-docker push cimg/go:1.17.11-node
-docker push cimg/go:1.17-browsers
-docker push cimg/go:1.17.11-browsers
+docker push ccitest/testing:1.17
+# docker push cimg/go:1.17.11
+# docker push cimg/go:1.17-node
+# docker push cimg/go:1.17.11-node
+# docker push cimg/go:1.17-browsers
+# docker push cimg/go:1.17.11-browsers
